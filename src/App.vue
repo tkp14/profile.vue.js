a445b71cef826160506d9655c44f70029d2fd3a2
tkp14 <tk.091438@gmail.com>
2021-04-01 21:32:08 +0900

    リセット

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <h2>My Profile</h2>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'App',
   components: {
